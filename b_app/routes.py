ba2de134260a97bd8fe88470a6142a43c9f29914
Mukeli3 <mukelicynthia20@gmail.com>
2024-07-09 15:32:09 +0000

    routes fix

--- b_app/routes.py
+++ b_app/routes.py
@@ -1,39 +1,36 @@
-from flask import Blueprint, render_template, redirect, url_for
-from flask import request, flash
-from b_app import app, db, bcrypt
-from b_app.models import User
-from flask_login import login_user, logout_user, login_required
+from flask import Blueprint, jsonify
+from b_app import app, db
+from b_app.models import Song, User
+from flask_login import login_required, current_user
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        user = User.query.filter_by(username=username).first()
-        if user and bcrypt.check_password_hash(user.password, password):
-            login_user(user)
-            return redirect(url_for('profile'))
-        else:
-            flash('Please confirm your credentials. Login unsuccessful', 'danger')
-        return render_template('index.html')
+routes = Blueprint('routes'. __name__)
 
-@auth.route('/register', method=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-        new_user = User(username=username, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        login_user(new_user)
-        return redirect(url_for('profile'))
-    return render_template('index.html')
 
-@auth.route('/logout')
+@routes.route('/songs')
 @login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
+def get_songs():
+    songs = Song.query.all()
+    return jsonify([{'title': song.title, 'artist': song.artist, 'album': song.album} for song in songs])
+
+@routes.route('/recommendations')
+@login_required
+def get_recommendations():
+    user_songs = Song.query.filter_by(user_id=current_user.id).all()
+    if not user_songs:
+        return ([])
+
+    user_feats = np.array([np.fromstring(song.feats, sep=',') for song in user_songs])
+    a_songs = Song.query.all()
+    a_feats= np.array([np.fromstring(song.feats, sep=',') for song in a_songs])
+
+    similarities = cosine_similarity(user_feats, a_feats)
+    recommendations = []
+    for index in np.argsort(-similarities, axis=1)[:10].flatten():
+        song = a_songs[index]
+        recommendations.append({'title': song.title, 'artist': song.artist, 'album': song.album})
+    return jsonify(recommendations)
