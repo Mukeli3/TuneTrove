22b3f76238164e43c010c24c5a4b876cc8d2b56c
Mukeli3 <mukelicynthia20@gmail.com>
2024-07-29 20:12:24 +0000

    fixed file structure a lil

--- b_app/__init__.py
+++ b_app/__init__.py
@@ -10,7 +10,13 @@ app.config.from_object('b_app.config')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'auth.login'
 login_manager.login_message = 'Please login to access TuneTrove'
 
-from b_app import routes, models
+from b_app.authent import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+
+from b_app.routes import routes as routes_blueprint
+app.register_blueprint(routes_blueprint)
+
+from b_app import models
